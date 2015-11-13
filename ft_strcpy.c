4a49ae104755e4f97962a3e0954e38f7811e8376
Maxime FORTIN <m.fortin75017@gmail.com>
2015-11-13 22:28:45 +0100

    Update ft_strcpy.c

--- ft_strcpy.c
+++ ft_strcpy.c
@@ -5,20 +5,13 @@ char	*ft_strcpy(char *dest, const char *src)
 	unsigned int	i;
 	char			*tmp;
 
-	tmp = (char *)malloc(sizeof(char) * ft_strlen(dest) + 1);
 	i = 0;
+	tmp = (char *)malloc(sizeof(char) * ft_strlen(dest) + 1);
 	while (src[i] != '\0')
 	{
 		tmp[i] = src[i];
 		i++;
 	}
-	i = 0;
-	while (tmp[i] != '\0')
-	{
-		dest[i] = tmp[i];
-		i++;
-	}
 	dest[i] = '\0';
-	free(tmp);
 	return (dest);
 }
