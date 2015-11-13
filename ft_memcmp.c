f8b8365c0613c755298e605265579add0c18f4c7
Maxime FORTIN <m.fortin75017@gmail.com>
2015-11-13 23:19:52 +0100

    Update ft_memcmp.c

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -9,6 +9,8 @@ int		ft_memcmp(const void *s1, const void *s2, size_t n)
 	str1 = (char *)s1;
 	str2 = (char *)s2;
 	i = 0;
+	if (n == 0)
+		return (0);
 	while (str1[i] == str2[i] && i < n - 1)
 		i++;
 	return ((unsigned char)str1[i] - (unsigned char)str2[i]);
