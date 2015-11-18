a5ce601765a324bab4c6d6e07f652febeb5a4b17
mfortin42 <m.fortin75017@gmail.com>
2015-11-18 22:41:19 +0100

    GNL functional

--- get_next_line.c
+++ get_next_line.c
@@ -1,40 +1,56 @@
-static int	ft_cat_buff_to_line(char **line, char *buff, char *tmp)
-{
-	unsigned int i;
+#include "get_next_line.h"
+#include <stdio.h>
+#include <fcntl.h>
 
-	i = 0;
-	if (tmp)
+int	main(int ac, char **av)
+{
+	char	*line;
+	int		fd;
+
+	(void)ac;
+	fd = open(av[1], O_RDONLY);
+	while (get_next_line(fd, &line))
 	{
-		while (buff[i] != '\n')
-			i++;
-		ft_strncat(*line, buff, i - 1);
-		ft_strclr(buff);
-		return (1);
+		printf("%s\n", line);
 	}
-	else
-	{
-		ft_strcat(*line, buff);
-		ft_strclr(buff);
-		return (0);
-	}
+	return (0);
 }
 
-int	ft_get_next_line(int fd, char **line)
+static size_t	ft_cnt_chr(char *tmp)
 {
-	static char	buff[BUFF_SIZE];
+	size_t	i;
+
+	i = 0;
+	while (tmp[i] != '\0' && tmp[i] != '\n')
+		i++;
+	return (i);
+}
+
+int	get_next_line(int fd, char **line)
+{
+	char		buff[BUFF_SIZE + 1];
 	char		*tmp;
+	static char	*tmp2 = "";
 	int			v;
 
-	v = 1;
-	if (*line == NULL)
+	tmp = tmp2;
+	if (fd < 0 || line == NULL)
 		return (-1);
-	ft_strclr(*line);
-	while (v)
+	while ((v = read(fd, buff, BUFF_SIZE)))
 	{
-		v = read(fd, buff, BUFF_SIZE);
-		tmp = ft_strchr(buff, '\n');
-		if (ft_join(*line, buff, tmp))
-			return (1);
+		buff[v] = '\0';
+		tmp = ft_strjoin(tmp, buff);
 	}
+	if (ft_strchr(tmp, '\n'))
+	{
+		tmp2 = ft_strdup(ft_strchr(tmp, '\n') + 1);
+		*line = ft_strsub(tmp, 0, ft_cnt_chr(tmp));
+		return (1);
+	}
+	*line = ft_strsub(tmp, 0, ft_cnt_chr(tmp));
+	if (ft_strchr(tmp, '\n'))
+		tmp2 = ft_strdup(ft_strchr(tmp, '\n') + 1);
+	if (ft_strchr(tmp, '\n'))
+		return (1);
 	return (0);
 }
