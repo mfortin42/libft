f1a03d5e0c8ea023e64ff2461519fa1457810202
Maxime FORTIN <m.fortin75017@gmail.com>
2015-11-19 23:27:44 +0100

    Update ft_memmove.c

--- ft_memmove.c
+++ ft_memmove.c
@@ -7,8 +7,8 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 	tmp = (char *)malloc(sizeof(char) * n);
 	if (tmp == NULL)
 		return (NULL);
-	tmp = ft_memcpy(tmp, src, n);
-	dest = ft_memcpy(dest, tmp, n);
+	ft_memcpy(tmp, src, n);
+	ft_memcpy(dest, tmp, n);
 	free(tmp);
 	return (dest);
 }
