0f4c32e4f944020fd323cbc4cdf1a2e22556fb6d
Maxime FORTIN <m.fortin75017@gmail.com>
2015-11-19 23:30:43 +0100

    Update ft_lstiter.c

--- ft_lstiter.c
+++ ft_lstiter.c
@@ -2,15 +2,10 @@
 
 void	ft_lstiter(t_list *lst, void (*f)(t_list *elem))
 {
-	t_list	*tmp;
-
 	if (f != NULL)
-	{
 		while (lst != NULL)
 		{
-			tmp = lst->next;
 			f(lst);
-			lst = tmp;
+			lst = lst->next;
 		}
-	}
 }
