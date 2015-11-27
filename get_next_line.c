a4aa1aca37d7d5b3f3eef942d810d78ef581f177
Maxime FORTIN <m.fortin75017@gmail.com>
2015-11-27 14:25:26 +0100

    Update get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -16,79 +16,23 @@
 	return (0);
 }*/
 
-static size_t	ft_cnt_chr(char *tmp) // Version 1.0
-{
-	size_t	i;
-
-	i = 0;
-	while (tmp[i] != '\0' && tmp[i] != '\n')
-		i++;
-	return (i);
-}
-
-int	get_next_line(int fd, char **line)
-{
-	char		buff[BUFF_SIZE + 1];
-	char		*tmp;
-	static char	*tmp2 = "";
-	int		v;
-
-	tmp = tmp2;
-	if (read(fd, buff, 1) == -1 || line == NULL)
-		return (-1);
-	while ((v = read(fd, buff, BUFF_SIZE)))
-	{
-		buff[v] = '\0';
-		tmp = ft_strjoin(tmp, buff);
-	}
-	if (ft_strchr(tmp, '\n'))
-	{
-		tmp2 = ft_strdup(ft_strchr(tmp, '\n') + 1);
-		*line = ft_strsub(tmp, 0, ft_cnt_chr(tmp));
-		return (1);
-	}
-	*line = ft_strsub(tmp, 0, ft_cnt_chr(tmp));
-	if (ft_strchr(tmp, '\n'))
-		tmp2 = ft_strdup(ft_strchr(tmp, '\n') + 1);
-	if (ft_strchr(tmp, '\n'))
-		return (1);
-	return (0);
-}
-
-static size_t	ft_cnt_chr(char *tmp) // Version 2.0
-{
-	size_t	i;
-
-	i = 0;
-	while (tmp[i] != '\0' && tmp[i] != '\n')
-		i++;
-	return (i);
-}
-
 int	get_next_line(int fd, char **line)
 {
 	char			buff[BUFF_SIZE + 1];
 	static char		*tmp = "";
-	char			*tmp2;
 	int				v;
 
 	v = 1;
-	while (ft_strchr(tmp, '\n') == NULL && v > 0)
+	while (ft_strchr(tmp, '\n') == NULL && (v = read(fd, buff, BUFF_SIZE) > 0))
 	{
-		if ((v = read(fd, buff, BUFF_SIZE)) == -1)
-			return (-1);
 		buff[v] = '\0';
-		tmp2 = ft_strjoin(tmp, buff);
-		free(tmp);
-		tmp = tmp2;
+		tmp = ft_strjoin(tmp, buff);
 	}
 	*line = ft_strsub(tmp, 0, ft_cnt_chr(tmp));
 	if (ft_strchr(tmp, '\n'))
 	{
-		tmp2 = ft_strdup(ft_strchr(tmp, '\n') + 1);
-		free(tmp);
-		tmp = tmp2;
+		tmp = ft_strchr(tmp, '\n') + 1;
 		return (1);
 	}
-	return (0);
+	return (v ? 1 : 0);
 }
